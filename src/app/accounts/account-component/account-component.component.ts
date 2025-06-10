328438337bd94c748ded5a4630527273c96023e9
Peggy <peggydudu5@gmail.com>
2025-06-10 10:47:23 +0200

    commit

---
+++ src/app/accounts/account-component/account-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-component',
+  imports: [],
+  templateUrl: './account-component.component.html',
+  styleUrl: './account-component.component.css'
+})
+export class AccountComponentComponent {
+
+}
---
+++ src/app/accounts/log-in-component/log-in-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-component',
+  imports: [],
+  templateUrl: './log-in-component.component.html',
+  styleUrl: './log-in-component.component.css'
+})
+export class LogInComponentComponent {
+
+}
