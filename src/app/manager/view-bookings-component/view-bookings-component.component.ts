f643fd9fd9d4e6f73933b45f80e9441d2b88c207
Peggy <peggydudu5@gmail.com>
2025-06-18 12:50:16 +0200

    Commit

--- src/app/manager/view-bookings-component/view-bookings-component.component.ts
+++ src/app/manager/view-bookings-component/view-bookings-component.component.ts
@@ -11,10 +11,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './view-bookings-component.component.html',
   styleUrl: './view-bookings-component.component.css'
 })
-<<<<<<< HEAD
-export class ViewBookingsComponentComponent {
-  
-=======
 export class ViewBookingsComponentComponent implements OnInit {
 
   booking :BookingForm[] =[]
@@ -35,7 +31,6 @@ export class ViewBookingsComponentComponent implements OnInit {
   get bookings(){
     return this.booking
   }
->>>>>>> e3504d0e6a8b30920e6e19cfe411abfaf0fcc0f6
    
 
 }
